8d656b4e1ed5c5a81cb79b857913e5111cbca9a1
The-Asgardian <77071320+The-Asgardian@users.noreply.github.com>
2023-12-07 15:25:53 +0000

    Update PC.sv

--- PC.sv
+++ PC.sv
@@ -4,14 +4,16 @@ module PC #(
     input   logic              clk,
     input   logic              rst,
     input   logic [WIDTH-1:0]  PCNext,
+    input   logic              Stall,
     output  logic [WIDTH-1:0]  PC
 );
 
     always_ff @ (posedge clk) begin
-        if (rst)
-            PC <= 32'b0;
-        else
-            PC <= PCNext;
+        if (~Stall)
+            if (rst)
+                PC <= 32'b0;
+            else
+                PC <= PCNext;
+        end
     end
-          
 endmodule
