74dd6c423c0007772150866643ae7f859621efdf
The-Asgardian <77071320+The-Asgardian@users.noreply.github.com>
2023-12-07 20:06:54 +0000

    Update RegE.sv

--- RegE.sv
+++ RegE.sv
@@ -7,7 +7,6 @@ module RegE (
     input logic  [4:0]          RdE,
     input logic  [31:0]         PCPlus4E,
     input logic                 clk,
-    input logic                 FlushE,
     
     output logic                 RegWriteM,
     output logic  [1:0]          ResultSrcM,
@@ -21,24 +20,13 @@ module RegE (
 
 always_ff @(negedge clk) begin
 
-    if(~FlushE) begin
-        // Only passing through values if Stall is 0
-        RegWriteM   <= RegWriteE;
-        ResultSrcM  <= ResultSrcE;
-        MemWriteM   <= MemWriteE;
-        ALUResultM  <= ALUResultE;
-        WriteDataM  <= WriteDataE;
-        RdM         <= RdE;
-        PCPlus4M    <= PCPlus4E;
-    end else begin
-        RegWriteM   <= 0;
-        ResultSrcM  <= 0;
-        MemWriteM   <= 0;
-        ALUResultM  <= 0;
-        WriteDataM  <= 0;
-        RdM         <= 0;
-        PCPlus4M    <= 0;
-    end
+    RegWriteM   <= RegWriteE;
+    ResultSrcM  <= ResultSrcE;
+    MemWriteM   <= MemWriteE;
+    ALUResultM  <= ALUResultE;
+    WriteDataM  <= WriteDataE;
+    RdM         <= RdE;
+    PCPlus4M    <= PCPlus4E;
 end
     
 endmodule
