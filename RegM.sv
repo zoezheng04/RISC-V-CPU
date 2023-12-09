13ae070b22ea7b79790ea7ca36201e8739860a15
The-Asgardian <77071320+The-Asgardian@users.noreply.github.com>
2023-12-09 16:36:46 +0000

    Update RegM.sv

--- RegM.sv
+++ RegM.sv
@@ -1,14 +1,14 @@
 module RegM (
     
     input logic             RegWriteM,
-    input logic  [1:0]      ResultSrcM,
+    input logic             ResultSrcM,
     input logic  [31:0]     ReadDataM,
     input logic  [4:0]      RdM,
     input logic  [31:0]     ALUResultM,
     input logic             clk,
 
     output logic            RegWriteW,
-    output logic  [1:0]     ResultSrcW,
+    output logic            ResultSrcW,
     output logic  [31:0]    ReadDataW,
     output logic  [4:0]     RdW,
     output logic  [31:0]    ALUResultW
