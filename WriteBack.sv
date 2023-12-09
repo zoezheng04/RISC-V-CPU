229288376d91d06df18428f899f388f083939ba0
The-Asgardian <77071320+The-Asgardian@users.noreply.github.com>
2023-12-09 16:04:34 +0000

    Update WriteBack.sv

--- WriteBack.sv
+++ WriteBack.sv
@@ -1,18 +1,11 @@
 module WriteBack (
     input logic          RegWriteW,
-    input logic [1:0]    ResultSrcW,
+    input logic          ResultSrcW,
     input logic [31:0]   ALUResultW,
     input logic [31:0]   ReadDataW,
-    input logic [31:0]   PCPlus4W,
 
     output logic [31:0]  ResultW
 );
+assign ResultW = ResultSrcW ? ReadDataW : ALUResultW;
     
-case (ResultSrcW)
-    2'b00: assign ResultW = ALUResultW;
-    2'b01: assign ResultW = ReadDataW;
-    2'b10: assign ResultW = PCPlus4W; 
-    default: ResultW = ALUResult;
-endcase
-
 endmodule
