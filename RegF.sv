702511b0a97a0a0e7ceab7f77e5a200365d60ad0
The-Asgardian <77071320+The-Asgardian@users.noreply.github.com>
2023-12-14 16:09:59 +0000

    Update RegF.sv

--- RegF.sv
+++ RegF.sv
@@ -3,7 +3,6 @@ module RegF (
     input logic [31:0]      PCPlus4F,
     input logic             clk,
     input logic             Stall,
-    input logic             FlushF,
 
     output logic [31:0]    InstrD,
     output logic [31:0]    PCPlus4D
@@ -11,13 +10,9 @@ module RegF (
 
     always_ff @( negedge clk) begin
 
-        if((Stall == 0) && (FlushF == 0)) begin
+        if(Stall == 0) begin
             InstrD   <= InstrF;
             PCPlus4D <= PCPlus4F;
-        end
-        if (FlushF) begin
-            InstrD   <= 0'h00000013;
-            PCPlus4D <= 0;
         end
     end
 
