fee9c0737f6e70bc70d1bd7d921491abfe5f32ef
The-Asgardian <77071320+The-Asgardian@users.noreply.github.com>
2023-12-14 13:34:15 +0000

    Update HazardUnit.sv

--- HazardUnit.sv
+++ HazardUnit.sv
@@ -12,15 +12,15 @@ module HazardUnit (
     input logic [4:0]       RdW,
     input logic             RegWriteW,
     input logic             ResultSrcE,
-    input logic             ResultSrcM, // ResultSrc can be used here
+    input logic             ResultSrcM, 
 
-    output logic  [1:0]     ForwardAE,  // Forward data to RD1E
-    output logic  [1:0]     ForwardBE,  // Forward data to RD2E
-    output logic            ForwardAD,  // 
-    output logic            ForwardBD,  //
+    output logic  [1:0]     ForwardAE,  
+    output logic  [1:0]     ForwardBE,  
+    output logic            ForwardAD,   
+    output logic            ForwardBD,  
     output logic            StallF,
-    output logic            StallD,
-    output logic            FlushE
+    output logic            StallPC,
+    output logic            FlushD
 );
 logic                BranchStall;
 logic                lwstall;
@@ -29,13 +29,17 @@ always_comb begin
     // Initialize signals to no forwarding
     ForwardAE = 2'b00;
     ForwardBE = 2'b00;
-    ForwardAD = ((Rs1D != 0) && (Rs1D == RdM) && RegWriteM);
-    ForwardBD = ((Rs2D != 0) && (Rs2D == RdM) && RegWriteM);
+
+    ForwardAD = ((Rs1D != 0) && (Rs1D == RdM) && RegWriteM); // Forward data from memory stage (M) to decode stage (D) for RD1D
+    ForwardBD = ((Rs2D != 0) && (Rs2D == RdM) && RegWriteM); // Forward data from memory stage (M) to decode stage (D) for RD2D
+
+    // Stall registers for branch instructions in the decode stage (D) and there are data dependencies in execute (E) or memory (M) stages
     BranchStall = (BranchD && RegWriteE && ((WriteRegE == Rs1D) || (WriteRegE == Rs2D))) || (BranchD && ResultSrcM && ((RdM == Rs1D) || (RdM == Rs2D)));
-    lwstall   = (RegWriteE && ResultSrcE);
-    StallD = (lwstall || BranchStall);
-    StallF = (lwstall || BranchStall);
-    FlushE = (lwstall);
+    lwstall   = ((RegWriteE && ResultSrcE)); // Stall when Load instruction is detected in execute stage (E)
+
+    StallPC = (lwstall || BranchStall);      // Stall registers (F) and (D) for load and branch hazards
+    StallF = (lwstall || BranchStall);      // Stall registers (F) and (D) for load and branch hazards
+    FlushD = (lwstall);                     // Flush register (E) for load hazards
     
         if ((Rs1E != 0) && RegWriteW && (RdW == Rs1E)) 
             ForwardAE = 2'b01; // Forward data from write-back stage (W) to execute stage (E) for RD1E
