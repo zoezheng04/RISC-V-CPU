6abe14cf081c345e593d3b6775af3310dcb91647
The-Asgardian <arjanh6@gmail.com>
2023-12-15 23:24:47 +0000

    Test Scripts

---
+++ triangle.sh
@@ -0,0 +1,16 @@
+sed -i 's/f1.mem/pdf.hex/' rtl/rom.sv
+
+sed -i 's/sine.mem/triangle.mem/' rtl/DataMemory.sv
+sed -i 's/gaussian.mem/triangle.mem/' rtl/DataMemory.sv
+sed -i 's/noisy.mem/triangle.mem/' rtl/DataMemory.sv
+
+sed -i 's/i % 8 == 0/i % 1 == 0/' pdf_tb.cpp
+
+rm -rf obj_dir
+rm -f  Vcpu.vcd
+
+verilator -Wall -cc --trace cpu.sv --exe pdf_tb.cpp -Irtl -Itest
+
+make -j -C obj_dir/ -f Vcpu.mk Vcpu
+
+obj_dir/Vcpu
